4836fca43b88092b99409ceb67787de394403630
Bralor <Bralor92@email.cz>
2018-05-22 14:31:45 +0200

    First Django project with view for event welcome-page, offering and additional links

--- events/views.py
+++ events/views.py
@@ -6,15 +6,20 @@ from django.http import HttpResponse
 # 	return HttpResponse("Hey Client, my app is running!")
 
 def event_listing(request):
+	'''
+	Main menu/offering
+	'''
+
 	html = '''
 	
 	<ul>
-		<li> Chill on the beach </li>
-		<li> Camping in the woods </li>
-		<li> Flying into the space </li>
+		<li> Chill on the beach <a href="/event/Chill">detail</a> </li>
+		<li> Camping in the woods <a href="/event/Camping">detail</a> </li>
+		<li> Flying into the space <a href="/event/Flying">detail</a> </li>
 	</ul>
 	'''
 	return HttpResponse(html)
+
 
 def index(request):
 	html = '''
@@ -24,3 +29,19 @@ def index(request):
 
 	'''
 	return HttpResponse(html)
+
+
+def event_detail(request, name):
+    data = {
+    	'Chill' : '<h2> Chill on the beach just for $400 </h2>',
+    	'Camping': '<h2> Camp with us for $50 </h2>',
+    	'Flying': '<h2> Fly for free </h2>'
+    		}
+    selection = data.get(name)
+
+    if selection:
+    	return HttpResponse(selection)
+    else:
+    	return HttpResponse('There is no such event in our offering')
+
+
