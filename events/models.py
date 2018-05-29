e4894f2bdd5e732acaeeaacf5221baa3c7e10890
Bralor <Bralor92@email.cz>
2018-05-29 10:56:32 +0200

    We have created a new table 'events' and model (class) Event (events/models.py)

--- events/models.py
+++ events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# first model = class with events
+class Event(models.Model):
+	"""docstring for ClassName"""
+	name        = models.CharField(max_length = 200)
+	description = models.TextField(max_length = 200)
+	location	= models.CharField(max_length = 500)
+	category	= models.CharField(max_length = 20)
