a67a24928b278dc3c4a33cee29a8628fa416595a
Bralor <Bralor92@email.cz>
2018-06-08 22:10:54 +0200

    We have added profile view into accounts.views. Added accounts.urls.py and templates.accounts.profile.html

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'explorea.events'
+    'explorea.events',
+    'explorea.accounts'
 ]
 
 MIDDLEWARE = [
