a76b88fd6e4e3ff3bf5881c7a0a775865ab92634
Bralor <Bralor92@email.cz>
2018-06-12 09:16:39 +0200

    We have added a change-password functionality = change_password view with, change_password.html template. We have also added Change password link to accounts/profile.html template

--- explorea/accounts/views.py
+++ explorea/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import RegisterForm, EditProfileForm
+from django.contrib.auth.forms import PasswordChangeForm
 
 def profile(request):
 	return render(request, 'accounts/profile.html')
+
 
 def register(request):
 	if request.method == 'POST':
@@ -22,6 +24,7 @@ def register(request):
 	form = RegisterForm()
 	return render(request, 'accounts/register.html', {'form': form})
 
+
 def edit_profile(request):
 	if request.method == 'POST':
 		form = EditProfileForm(request.POST, instance=request.user)
@@ -33,4 +36,20 @@ def edit_profile(request):
 
 	form = EditProfileForm(instance=request.user)
 	return render(request, 'accounts/edit_profile.html', {'form': form})
+
+
+def change_password(request):
+	''' The user wants to change his current password'''
+	if request.method == 'POST':
+		form = PasswordChangeForm(data=request.POST, user=request.user)
+		if form.is_valid():
+			user = form.save()
+			# we need to obtain a new session hash due to new password
+			update_session_auth_hash(request, user)
+			return redirect('accounts/profile/')
+	
+	# GET method
+	else:
+		form = PasswordChangeForm(user=request.user)
+		return render(request, 'accounts/change_password.html', {'form': form})
 
