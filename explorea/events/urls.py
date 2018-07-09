ef182d3eb3bc3c74522ea3c69eb4239dd6d7346c
Bralor <Bralor92@email.cz>
2018-07-09 12:38:46 +0200

    We have reordered our URLs.

--- explorea/events/urls.py
+++ explorea/events/urls.py
@@ -6,13 +6,13 @@ app_name = 'events'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('events/', views.event_listing, name='events'),
-    path('events/new/', views.create_event, name='create_event'),
-    path('events/detail/<int:pk>', views.event_detail, name='event_detail'),
-    path('events/update/<int:pk>', views.update_event, name='update_event'),
-    path('events/delete/<int:pk>', views.delete_event, name='delete_event'),
-    path('events/my_events', views.my_events, name='my_events'),
-    path('events/<int:event_id>/new_run/', views.create_event_run, name='create_event_run'),
-    path('events/update_run/<int:event_run_id>', views.update_event_run,name='update_event_run'),
-    path('events/delete_run/<int:event_run_id>', views.delete_event_run,name='delete_event_run'),
+    path('all/', views.event_listing, name='events'),
+    path('new/', views.create_event, name='create_event'),
+    path('my_events/', views.my_events, name='my_events'),
+    path('detail/<int:pk>', views.event_detail, name='event_detail'),
+    path('update/<int:pk>', views.update_event, name='update_event'),
+    path('delete/<int:pk>', views.delete_event, name='delete_event'),
+    path('<int:event_id>/new_run/', views.create_event_run, name='create_event_run'),
+    path('update_run/<int:event_run_id>', views.update_event_run,name='update_event_run'),
+    path('delete_run/<int:event_run_id>', views.delete_event_run,name='delete_event_run'),
                 ]
