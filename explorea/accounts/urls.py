5a54137ba6fc7cd5f14717080894c64cd72bcca6
Bralor <Bralor92@email.cz>
2018-07-10 09:58:54 +0200

    We have added a new view of login page.

--- explorea/accounts/urls.py
+++ explorea/accounts/urls.py
@@ -18,13 +18,14 @@ urlpatterns = 	[
 	path('profile/', views.profile, name='profile'),
 	path('profile/edit/', views.edit_profile, name='edit_profile'),
 	path('change-password/', views.change_password, name='change_password'),
+	path('reset_password/complete', password_reset_complete, name='password_reset_complete'),
+	path('reset-password/done/', password_reset_done, name='password_reset_done'),
 	path('reset-password/', password_reset,{'template_name': 'accounts/reset_password.html',
 		'post_reset_redirect': 'password_reset_done',
 		'email_template_name': 'accounts/reset_password_email.html'},
 		name='reset_password'),
-	path('reset-password/done/', password_reset_done, name='password_reset_done'),
 	url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm,
 		{'post_reset_redirect': 'password_reset_complete'}, name='password_reset_confirm'),
-	path('reset_password/complete', password_reset_complete, name='password_reset_complete'),
+	
 				
 				]
--- explorea/events/urls.py
+++ explorea/events/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     path('all/', views.event_listing, name='events'),
     path('new/', views.create_event, name='create_event'),
     path('my_events/', views.my_events, name='my_events'),
+    
     path('detail/<int:pk>', views.event_detail, name='event_detail'),
     path('update/<int:pk>', views.update_event, name='update_event'),
     path('delete/<int:pk>', views.delete_event, name='delete_event'),
+    
     path('<int:event_id>/new_run/', views.create_event_run, name='create_event_run'),
     path('update_run/<int:event_run_id>', views.update_event_run,name='update_event_run'),
     path('delete_run/<int:event_run_id>', views.delete_event_run,name='delete_event_run'),
